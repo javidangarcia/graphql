7b234e9aa04b335208c64795974afb8b259ef8f4
Javier Garcia <javierdangarcia@gmail.com>
2024-02-29 16:34:46 -0500

    added basic resolver functions

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema.js";
+import db from "./db.js";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        authors() {
+            return db.authors;
+        },
+        reviews() {
+            return db.reviews;
+        },
+    },
+};
 
 const server = new ApolloServer({
     typeDefs,
-    // resolvers,
+    resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
